099f739c8468fe72f8e92d5d1c452157b71107be
kHuynh05 <Kenrich6118@gmail.com>
2025-02-22 11:36:58 -0600

    Created support link section

--- public/index.php
+++ public/index.php
@@ -10,27 +10,61 @@
         </div>
     </div>
     <div class='homePageInfo'>
-        <div class='featuredAnimals'>
-            <div class = 'featured'>
-                <h1 class='featureIntro'>MEET <br> OUR <br>FEATURED <br>ANIMALS</h1>
-                <div class='feature'>
-                    <img class="featureimage"/>
-                    <h1 class="featuretitle">Red Panda</h1>
-                    <span class="featuredescription">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</span>
+        <div class='featured'>
+            <h1 class='featureIntro'>MEET <br> OUR <br>FEATURED <br>ANIMALS</h1>
+            <div class='feature'>
+                <img class="featureimage" />
+                <h1 class="featuretitle">Red Panda</h1>
+                <span class="featuredescription">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</span>
+            </div>
+            <div class='feature'>
+                <img class="featureimage" />
+                <h1 class="featuretitle">Red Panda</h1>
+                <span class="featuredescription">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</span>
+            </div>
+            <div class='feature'>
+                <img class="featureimage" />
+                <h1 class="featuretitle">Red Panda</h1>
+                <span class="featuredescription">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</span>
+            </div>
+        </div>
+        <div class="supportways">
+            <h1 class="support-title">How you can support!</h1>
+            <div class="support">
+                <div class="support-1">
+                    <div class="donate-homepage">
+                        <h1>Donate Today</h1>
+                        <span>Your generosity helps us care for the animals,sjfjweifjsidfjosiefjosiejfofjoisejfoefjs</span>
+                        <a href="/ZooApp/public/donation.php">
+                            <button class="frontPageButton">Donate Now</button>
+                        </a>
+                    </div>
+                    <div class="member-homepage">
+                        <h1>Become a member</h1>
+                        <span>Join us in our mission to protect, preserve, and celebrate wildlife! As a member of Zootopia, you'll gain exclusive benefits while directly supporting the care and conservation of animals from around the world.</span>
+                        <a href="/ZooApp/public/membership.php">
+                            <button class="frontPageButton">Discover Benefits</button>
+                        </a>
+                    </div>
                 </div>
-                <div class='feature'>
-                    <img class="featureimage"/>
-                    <h1 class="featuretitle">Red Panda</h1>
-                    <span class="featuredescription">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</span>
-                </div>
-                <div class='feature'>
-                    <img class="featureimage"/>
-                    <h1 class="featuretitle">Red Panda</h1>
-                    <span class="featuredescription">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</span>
+                <div class="support-2">
+                    <div class="donate-homepage">
+                        <h1>Be part of our community</h1>
+                        <span>Join us in our mission to protect, preserve, and celebrate wildlife! As a member of Zootopia, you'll gain exclusive benefits while directly supporting the care and conservation of animals from around the world.</span>
+                        <a href="/ZooApp/public/community.php">
+                            <button class="frontPageButton">Discover Benefits</button>
+                        </a>
+                    </div>
+                    <div class="member-homepage">
+                        <h1>Adopt an animal</h1>
+                        <span>Join us in our mission to protect, preserve, and celebrate wildlife! As a member of Zootopia, you'll gain exclusive benefits while directly supporting the care and conservation of animals from around the world.</span>
+                        <a href="/ZooApp/public/adopt.php">
+                            <button class="frontPageButton">Discover Benefits</button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <?php include('../includes/footer.php'); ?>
 </div>
-
