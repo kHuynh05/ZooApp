8e3e9d7cf076e8938111232ac2753de3092a2185
A8E1 <98847922+A8E1@users.noreply.github.com>
2025-03-08 12:31:41 -0600

    renamed 'animals.php' links to 'animals-info.php'. This makes it easier to distinguish from individual animal.php pages.
    also created the animals-info.php page to begin coding. Will be using the animals page within Houston Zoo for inspiration

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,5 @@
 <!-- navbar.php -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,7 @@
 <nav>
     <ul>
         <li><a href="homepage.php">Home</a></li>
-        <li><a href="animals.php">Animals</a></li>
+        <li><a href="animals-info.php">animals-info</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
     </ul>
