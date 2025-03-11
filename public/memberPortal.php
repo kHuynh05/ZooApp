c9b47b98fae9a2ba1890f3bebb8af1e6556948cf
kHuynh05 <Kenrich6118@gmail.com>
2025-03-11 18:34:31 -0500

    Started memberPortal

--- public/memberPortal.php
+++ public/memberPortal.php
@@ -1,9 +1,15 @@
 <?php
-// Check if the "jwt" cookie exists
-if (isset($_COOKIE['jwt_token'])) {
-    $jwt_token = $_COOKIE['jwt_token'];  // Retrieve the token
-    echo "JWT Token: " . $jwt_token;  // You can use the token as needed
-} else {
-    echo "No JWT token found. Please log in.";
-}
+// Start the session
+include '../config/database.php';
+include '../scripts/authorize.php';
 ?>
+<div class="container">
+    <div class="tabs">
+        <h1>Membership Portal</h1>
+        <div class="tab active" onclick="showTab(1)">Dashboard</div>
+        <div class="tab" onclick="showTab(2)">Membership</div>
+        <div class="tab" onclick="showTab(3)">Tickets</div>
+        <div class="tab" onclick="showTab(4)">Rewards</div>
+    </div>
+    <div></div>
+</div>
