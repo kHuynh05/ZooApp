6c00dac89053d813faa3634891d423409ad5e9ed
A8E1 <98847922+A8E1@users.noreply.github.com>
2025-03-22 09:13:03 -0500

    created SQL query to dynamically pull column info. using table name

--- public/event_insert.php
+++ public/event_insert.php
@@ -1,6 +1,40 @@
-//admin checker logic!!
+//admin checker logic!! GOES HERE
 
 <?php
+// Include database connection
+include '../config/database.php';
+
+$sqlForEventFields = "SELECT 
+    cols.COLUMN_NAME,
+    cols.DATA_TYPE,
+    cols.IS_NULLABLE,
+    cols.COLUMN_KEY,
+    refs.REFERENCED_TABLE_NAME,
+    refs.REFERENCED_COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS AS cols
+LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS refs
+    ON cols.TABLE_SCHEMA = refs.TABLE_SCHEMA
+    AND cols.TABLE_NAME = refs.TABLE_NAME
+    AND cols.COLUMN_NAME = refs.COLUMN_NAME
+    AND refs.REFERENCED_TABLE_NAME IS NOT NULL
+WHERE cols.TABLE_NAME = 'EVENTS'
+  AND cols.TABLE_SCHEMA = 'ZOOAPP';
+";
+
+$result = $conn->query($sqlForAnimals);
+
+
+// Check if there are any animals in the result
+if ($result->num_rows > 0) {
+    // Loop through the results and display each animal
+    $eventFields = [];
+    while ($row = $result->fetch_assoc()) {
+        $eventFields[] = $row;
+    }
+} else {
+    // Handle the case if failure to get event field
+    $eventFields = ['event_id' => null];
+}
 
 ?>
 
