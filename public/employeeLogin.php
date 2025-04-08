e8005c71df823160cf19220747eef3834cc38884
kHuynh05 <Kenrich6118@gmail.com>
2025-04-08 17:00:59 -0500

    forget semicolon

--- public/employeeLogin.php
+++ public/employeeLogin.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: employeePortal.php");
         exit();
     } else {
-        $message = "Invalid password or email"
+        $message = "Invalid password or email";
     }
 
     $stmt->close();
