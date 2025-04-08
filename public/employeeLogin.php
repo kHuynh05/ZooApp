5eefafbb2a7d04e51858523ef62ea6a2f30a4ad8
kHuynh05 <Kenrich6118@gmail.com>
2025-04-08 16:58:03 -0500

    fixed login

--- public/employeeLogin.php
+++ public/employeeLogin.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: employeePortal.php");
         exit();
     } else {
-        $message = "Invalid password or email" . $emp_id;
+        $message = "Invalid password or email"
     }
 
     $stmt->close();
